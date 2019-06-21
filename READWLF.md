1fa784849fe693175d348c0fe858b9576fff6524
Willian F <457361448@qq.com>
2019-06-21 14:15:36 +0800

    20190621,在主页面添加了fragment，fragment里有recyclerview

---
+++ READWLF.md
@@ -0,0 +1,3 @@
+#1.0.1 设计app图标，修改app_name为ECoach
+#1.0.2 TODO 修改点击选择图片按钮时的默认本地图片路径。
+#0619 添加RecycerView
--- app/src/main/java/cn/hzw/doodledemo/MainActivity.java
+++ app/src/main/java/cn/hzw/doodledemo/MainActivity.java
@@ -1,42 +1,66 @@
 package cn.hzw.doodledemo;
 
 import android.app.Activity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import cn.forward.androids.utils.LogUtil;
 import cn.hzw.doodle.DoodleActivity;
 import cn.hzw.doodle.DoodleParams;
 import cn.hzw.doodle.DoodleView;
 import cn.hzw.doodledemo.guide.DoodleGuideActivity;
+import cn.hzw.doodledemo.wlf.background;
+import cn.hzw.doodledemo.wlf.backgroundAdapter;
+import cn.hzw.doodledemo.wlf.recyclerviewFragment;
 import cn.hzw.imageselector.ImageLoader;
 import cn.hzw.imageselector.ImageSelectorActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     public static final int REQ_CODE_SELECT_IMAGE = 100;
     public static final int REQ_CODE_DOODLE = 101;
     private TextView mPath;
+    private List<background> backgroundList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        replaceFragment(new recyclerviewFragment());//wlf 0621
 
+        /*
+        wlf 0620
+
+       // initBackground();
+        recyclerviewFragment recyclerviewFragment = (RecyclerView) getSupportFragmentManager()
+            .findFragmentById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        backgroundAdapter adapter = new backgroundAdapter(backgroundList);
+        recyclerView.setAdapter(adapter);
         findViewById(R.id.btn_select_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ImageSelectorActivity.startActivityForResult(REQ_CODE_SELECT_IMAGE, MainActivity.this, null, false);
             }
         });
-
+         */
+        /*
         findViewById(R.id.btn_guide).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +80,7 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(getApplicationContext(), ScaleGestureItemDemo.class));
             }
         });
+        */
         mPath = (TextView) findViewById(R.id.img_path);
     }
 
@@ -99,5 +124,16 @@ public class MainActivity extends Activity {
                 Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    /*
+    wlf 0621
+     */
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.recycler_layout,fragment);
+        transaction.commit();
+
     }
 }
